16ad4a28b23709e181e3b45bfc6dbe50501b6622
sai-myne <ahlusir@gmail.com>
2020-07-20 12:11:08 +0800

    Pre-Caching Assets

--- sw.js
+++ sw.js
@@ -1,7 +1,29 @@
 self.skipWaiting()
+
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-    console.log("service worker has been installed");
+    // console.log("service worker has been installed");
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+    
 });
 
 // activate event
