a24380f335ac41af2adb5b6e659fff77a397d49e
sai-myne <ahlusir@gmail.com>
2020-07-20 13:51:53 +0800

    Limiting Cache Size

--- sw.js
+++ sw.js
@@ -16,6 +16,17 @@ const assets = [
     '/pages/fallback.html'
 ];
 
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if(keys.length > size){
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        })
+    })
+};
+
 // install service worker
 self.addEventListener('install', evt => {
     // console.log("service worker has been installed");
@@ -50,6 +61,7 @@ self.addEventListener('fetch', evt => {
             return cacheRes || fetch(evt.request).then(fetchRes => {
                 return caches.open(dynamicCacheName).then(cache => {
                     cache.put(evt.request.url, fetchRes.clone());
+                    limitCacheSize(dynamicCacheName, 15);
                     return fetchRes;
                 })
             });
